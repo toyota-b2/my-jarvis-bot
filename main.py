358adad7f2bb8940b57ea3c636e60030f32596a0
toyota-b2 <antouanbal12@gmail.com>
2026-09-19 22:14:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ logging.basicConfig(level=logging.INFO)
 # Σύνδεση με το Gemini API
 GEMINI_KEY = os.environ.get("GEMINI_API_KEY")
 genai.configure(api_key=GEMINI_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-2.5-flash')
+
 
 # Εντολή /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
